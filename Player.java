d133d544c88edfa6e089bc79bfdab73c2badce12
samfrack <89489598+samfrack@users.noreply.github.com>
2021-10-07 17:36:58 -0500

    Finished activity 7.2

--- Player.java
+++ Player.java
@@ -34,6 +34,9 @@ public class Player extends Actor
         NEXT_LEVEL = nextLevel;
         MUSIC = music;
         
+        healthCount = maxHealth;
+        health = new Health[maxHealth];   
+        
         STANDING_IMAGE = getImage();
         WALK_ANIMATION = new GreenfootImage[]
                          {
@@ -60,8 +63,15 @@ public class Player extends Actor
         gameOver();
     }
     
-    public void addedToWord(World world)
-    {}
+    public void addedToWorld(World world)
+    {
+        health[0] = new Health();
+        world.addObject(health[0], 30, 36);
+        health[1] = new Health();
+        world.addObject(health[1], 72, 36);
+        health[2] = new Health();
+        world.addObject(health[2], 114, 36);
+    }
     
     private void walk() 
     {
@@ -165,6 +175,8 @@ public class Player extends Actor
         if(isTouching(Obstacle.class))
         {
             removeTouching(Obstacle.class);
+            getWorld().removeObject(health[healthCount - 1]);
+            healthCount--;
         }
         
         // hit platform but not on ground
@@ -182,7 +194,13 @@ public class Player extends Actor
             WALK_ANIMATION[i].mirrorHorizontally();
         }
     }
-    private void gameOver() {}
+    private void gameOver() 
+    {
+        if(healthCount == 0)
+        {
+            Greenfoot.setWorld(new Level1());
+        }
+    }
     private boolean isOnGround() 
     {
         Actor ground = getOneObjectAtOffset(0, getImage().getHeight() / 2, Platform.class);
